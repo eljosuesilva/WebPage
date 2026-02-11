885d74bf98b98da1466463a5f837321c68ca4510
Josué <josuef.silva04@gmail.com>
2026-02-11 23:35:20 +0100

    015

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -8,16 +8,16 @@ export const Hero = () => {
                     <h1
                         className={[
                             shipporiMincho.className,
-                            "max-w-[700px] text-[#0a1628]",
-                            "text-[25px] leading-[1.08] tracking-[-0.012em]",
-                            "sm:text-[30px] sm:leading-[1.04]",
-                            "md:text-[30px] md:leading-[1.02]",
-                            "lg:text-[40px] lg:leading-[1] lg:-translate-x-35",
+                            "max-w-[800px] text-[#0a1628]",
+                            "text-[40px] leading-[1.08] tracking-[-0.012em]",
+                            "sm:text-[40px] sm:leading-[1.04]",
+                            "md:text-[40px] md:leading-[1.02]",
+                            "lg:text-[50px] lg:leading-[1] lg:-translate-x-35",
                         ].join(" ")}
                     >
                         <span className="block whitespace-nowrap">The frontier AI Lab</span>
-                        <span className="block whitespace-nowrap">building the first in-production</span>
-                        <span className="block whitespace-nowrap">Universal Geospatial Model.</span>
+                        <span className="mt-[0.16em] block whitespace-nowrap">building the first in-production</span>
+                        <span className="mt-[0.16em] block whitespace-nowrap">Universal Geospatial Model.</span>
                     </h1>
                 </div>
             </div>
